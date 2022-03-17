95ef44c5a3c0169236d8dca336e6fc3a78d68b1c
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-03-17 16:37:11 +0530

    Add files via upload

---
+++ Linux_Int_Q.4.c
@@ -0,0 +1,18 @@
+// LINUX INTERNAL //
+// Q.4 //
+// MAHADEV G I //
+
+
+
+#include<stdio.h>
+#include<unistd.h>
+
+int main()
+{
+	printf("This is the program for replacing new command line program\n");
+
+	execl("/bin/ls", "ls", "-lh", "-a" , 0);
+
+
+	return 0;
+}
